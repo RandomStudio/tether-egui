8bc0d5ceb6dca3bfbce3cd587a14bf6978b8f8f6
stephen <stephen.buchanan@random.studio>
2023-05-10 15:39:22 +0200

    moved most ui -rendering out of main

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@
 use clap::Parser;
 use settings::Cli;
 use std::fs;
-use ui::{available_widgets, standard_spacer};
+use ui::{available_widgets, general_agent_area, standard_spacer, widget_entries};
 
 extern crate rmp_serde;
 extern crate rmpv;
 extern crate serde_json;
 
 use eframe::egui;
-use egui::{Color32, RichText, Slider};
 use env_logger::Env;
 use insights::Insights;
 use log::{error, info, warn};
@@ -132,56 +131,6 @@ impl Model {
         self.next_topic = format!("{role}/{id}/{plug_name}");
         self.use_custom_topic = false;
     }
-
-    fn common_widget_values(&mut self, ui: &mut egui::Ui) {
-        ui.horizontal(|ui| {
-            ui.label("Name");
-            if ui
-                .text_edit_singleline(&mut self.next_widget.name)
-                .changed()
-            {
-                let shortened_name = String::from(self.next_widget.name.replace(' ', "_").trim());
-                self.next_widget.plug.name = shortened_name.clone();
-                if !self.use_custom_topic {
-                    let (role, id) = self.tether_agent.description();
-                    self.next_topic = format!("{role}/{id}/{}", shortened_name);
-                }
-            }
-        });
-        ui.horizontal(|ui| {
-            ui.label("Description");
-            ui.text_edit_singleline(&mut self.next_widget.description);
-        });
-        ui.horizontal(|ui| {
-            ui.label("Plug Name");
-            if ui
-                .text_edit_singleline(&mut self.next_widget.plug.name)
-                .changed()
-                && !self.use_custom_topic
-            {
-                let (role, id) = self.tether_agent.description();
-                let plug_name = self.next_widget.plug.name.clone();
-                self.next_topic = format!("{role}/{id}/{plug_name}");
-            }
-        });
-        ui.horizontal(|ui| {
-            if ui
-                .checkbox(&mut self.use_custom_topic, "Use custom topic")
-                .changed()
-                && !self.use_custom_topic
-            {
-                let (role, id) = self.tether_agent.description();
-                let plug_name = self.next_widget.plug.name.clone();
-                self.next_topic = format!("{role}/{id}/{plug_name}");
-            }
-        });
-        ui.add_enabled_ui(self.use_custom_topic, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Topic");
-                ui.text_edit_singleline(&mut self.next_topic);
-            });
-        });
-    }
 }
 
 enum QueueItem {
@@ -202,104 +151,8 @@ impl eframe::App for Model {
             }
         }
 
-        egui::SidePanel::left("Settings").show(ctx, |ui| {
-            ui.heading("Tether Agent");
-
-            standard_spacer(ui);
-            ui.separator();
-            ui.heading("Load/Save");
-            ui.horizontal(|ui| {
-                if ui.button("Save").clicked() {
-                    let text = serde_json::to_string_pretty(&self.widgets)
-                        .expect("failed to serialise widget data");
-                    match fs::write("./widgets.json", text) {
-                        Ok(()) => {
-                            info!("Saved OK");
-                        }
-                        Err(e) => {
-                            error!("Error saving to disk: {:?}", e);
-                        }
-                    }
-                }
-                if ui.button("Load").clicked() {
-                    self.widgets = load_widgets_from_disk();
-                }
-                if ui.button("Clear").clicked() {
-                    self.widgets.clear();
-                }
-            });
-
-            standard_spacer(ui);
-            ui.separator();
-            ui.heading("Agent");
-
-            ui.label(self.tether_agent.broker_uri());
-
-            if self.tether_agent.is_connected() {
-                ui.label(RichText::new("Connected ☑").color(Color32::GREEN));
-            } else {
-                ui.label(RichText::new("Not connected ✖").color(Color32::RED));
-                if ui.button("Connect").clicked() {
-                    self.tether_agent.connect();
-                    self.insights = Insights::new(&self.tether_agent, false);
-                }
-            }
-
-            ui.separator();
-
-            ui.horizontal(|ui| {
-                ui.label("Role");
-                if ui.text_edit_singleline(&mut self.agent_role).changed() {
-                    self.tether_agent.set_role(&self.agent_role);
-                    self.prepare_next_entry();
-                }
-            });
-            ui.horizontal(|ui| {
-                ui.label("ID or Group");
-                if ui.text_edit_singleline(&mut self.agent_id).changed() {
-                    self.tether_agent.set_id(&self.agent_id);
-                    self.prepare_next_entry();
-                }
-            });
-
-            standard_spacer(ui);
-            ui.separator();
-            ui.heading("Insights");
-            ui.checkbox(&mut self.continuous_mode, "Continuous mode");
-            ui.label(format!("Topics x{}", self.insights.topics().len()));
-            for t in self.insights.topics() {
-                ui.small(t);
-            }
-            ui.separator();
-            ui.label(format!("Plug Names x{}", self.insights.plugs().len()));
-            for t in self.insights.plugs() {
-                ui.small(t);
-            }
-            ui.separator();
-            ui.label(format!("Agent Roles x{}", self.insights.roles().len()));
-            for t in self.insights.roles() {
-                ui.small(t);
-            }
-            ui.separator();
-            ui.label(format!("Agent IDs (groups) x{}", self.insights.ids().len()));
-            for t in self.insights.ids() {
-                ui.small(t);
-            }
-
-            standard_spacer(ui);
-            ui.separator();
-            ui.heading(format!("Messages x{}", self.insights.message_count()));
-            if self.insights.message_log().is_empty() {
-                ui.small("0 messages received");
-            }
-            egui::ScrollArea::vertical()
-                .auto_shrink([false; 2])
-                .show(ui, |ui| {
-                    for (topic, json) in self.insights.message_log().iter().rev() {
-                        ui.colored_label(Color32::LIGHT_BLUE, topic);
-                        ui.label(json);
-                    }
-                });
+        egui::SidePanel::left("General").show(ctx, |ui| {
+            general_agent_area(ui, self);
         });
 
         egui::SidePanel::right("Custom UI")
@@ -313,101 +166,7 @@ impl eframe::App for Model {
                 egui::ScrollArea::vertical()
                     .auto_shrink([false; 2])
                     .show(ui, |ui| {
-                        for (i, entry) in self.widgets.iter_mut().enumerate() {
-                            ui.separator();
-
-                            match entry {
-                                WidgetEntry::FloatNumber(e) => {
-                                    let (min, max) = e.range();
-                                    let heading =
-                                        format!("Number: {} ({}..={})", e.common().name, min, max);
-                                    entry_heading(ui, heading);
-                                    if ui
-                                        .add(
-                                            Slider::new(e.value_mut(), min..=max)
-                                                .clamp_to_range(false),
-                                        )
-                                        .changed()
-                                    {
-                                        if self.tether_agent.is_connected() {
-                                            self.tether_agent
-                                                .encode_and_publish(&e.common().plug, e.value())
-                                                .expect("Failed to send number");
-                                        }
-                                    };
-                                    entry_footer(ui, e);
-                                }
-                                WidgetEntry::WholeNumber(e) => {
-                                    let (min, max) = e.range();
-                                    let heading =
-                                        format!("Number: {} ({}..={})", e.common().name, min, max);
-                                    entry_heading(ui, heading);
-                                    if ui
-                                        .add(
-                                            Slider::new(e.value_mut(), min..=max)
-                                                .clamp_to_range(false),
-                                        )
-                                        .changed()
-                                    {
-                                        if self.tether_agent.is_connected() {
-                                            self.tether_agent
-                                                .encode_and_publish(&e.common().plug, e.value())
-                                                .expect("Failed to send number");
-                                        }
-                                    };
-                                    entry_footer(ui, e);
-                                }
-                                WidgetEntry::Colour(e) => {
-                                    entry_heading(ui, format!("Colour: {}", e.common().name));
-                                    if ui
-                                        .color_edit_button_srgba_unmultiplied(e.value_mut())
-                                        .changed()
-                                    {
-                                        if self.tether_agent.is_connected() {
-                                            self.tether_agent
-                                                .encode_and_publish(&e.common().plug, e.value())
-                                                .expect("Failed to send colour")
-                                        }
-                                    };
-                                    let srgba = e.value();
-                                    ui.label(format!(
-                                        "sRGBA: {} {} {} {}",
-                                        srgba[0], srgba[1], srgba[2], srgba[3],
-                                    ));
-                                    entry_footer(ui, e);
-                                }
-                                WidgetEntry::Bool(e) => {
-                                    entry_heading(ui, format!("Boolean: {}", e.common().name));
-                                    let checked = *e.value();
-                                    if ui
-                                        .checkbox(
-                                            e.value_mut(),
-                                            format!("State: {}", {
-                                                if checked {
-                                                    "TRUE"
-                                                } else {
-                                                    "FALSE "
-                                                }
-                                            }),
-                                        )
-                                        .changed()
-                                    {
-                                        if self.tether_agent.is_connected() {
-                                            self.tether_agent
-                                                .encode_and_publish(&e.common().plug, e.value())
-                                                .expect("Failed to send boolean");
-                                        }
-                                    }
-                                    entry_footer(ui, e);
-                                }
-                            }
-
-                            if ui.button("❌ Remove").clicked() {
-                                self.queue.push(QueueItem::Remove(i));
-                            }
-
-                            standard_spacer(ui);
-                        }
+                        widget_entries(ui, self);
                     });
             });
 
@@ -415,15 +174,4 @@ impl eframe::App for Model {
             available_widgets(ctx, self);
         });
     }
-}
-
-fn entry_heading(ui: &mut egui::Ui, heading: String) {
-    ui.label(RichText::new(heading).color(Color32::WHITE));
-}
-
-fn entry_footer<T>(ui: &mut egui::Ui, entry: &impl Widget<T>) {
-    ui.small(&entry.common().description);
-    ui.label(
-        RichText::new(&format!("Topic: {}", entry.common().plug.topic)).color(Color32::LIGHT_BLUE),
-    );
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
+use crate::{insights::Insights, load_widgets_from_disk, QueueItem, Widget};
+use egui::{Color32, RichText, Slider, Ui};
+use log::{error, info};
+
 use crate::{
     widgets::{BoolWidget, ColourWidget, NumberWidget},
     Model, WidgetEntry,
@@ -7,9 +13,20 @@ pub fn standard_spacer(ui: &mut egui::Ui) {
     ui.add_space(16.);
 }
 
+pub fn entry_heading(ui: &mut egui::Ui, heading: String) {
+    ui.label(RichText::new(heading).color(Color32::WHITE));
+}
+
+pub fn entry_footer<T>(ui: &mut egui::Ui, entry: &impl Widget<T>) {
+    ui.small(&entry.common().description);
+    ui.label(
+        RichText::new(&format!("Topic: {}", entry.common().plug.topic)).color(Color32::LIGHT_BLUE),
+    );
+}
+
 pub fn available_widgets(ctx: &egui::Context, model: &mut Model) {
     egui::Window::new("Floating-Point Number").show(ctx, |ui| {
-        model.common_widget_values(ui);
+        common_widget_values(ui, model);
 
         standard_spacer(ui);
 
@@ -56,7 +73,7 @@ pub fn available_widgets(ctx: &egui::Context, model: &mut Model) {
     });
 
     egui::Window::new("Whole Number").show(ctx, |ui| {
-        model.common_widget_values(ui);
+        common_widget_values(ui, model);
 
         standard_spacer(ui);
 
@@ -105,7 +122,7 @@ pub fn available_widgets(ctx: &egui::Context, model: &mut Model) {
     });
 
     egui::Window::new("Colour").show(ctx, |ui| {
-        model.common_widget_values(ui);
+        common_widget_values(ui, model);
         ui.separator();
         if ui.button("✚ Add").clicked() {
             model.widgets.push(WidgetEntry::Colour(ColourWidget::new(
@@ -133,7 +150,7 @@ pub fn available_widgets(ctx: &egui::Context, model: &mut Model) {
     });
 
     egui::Window::new("Boolean").show(ctx, |ui| {
-        model.common_widget_values(ui);
+        common_widget_values(ui, model);
         ui.separator();
         if ui.button("✚ Add").clicked() {
             model.widgets.push(WidgetEntry::Bool(BoolWidget::new(
@@ -158,5 +175,252 @@ pub fn available_widgets(ctx: &egui::Context, model: &mut Model) {
             )));
             model.prepare_next_entry();
         }
+    });
+}
+
+pub fn widget_entries(ui: &mut Ui, model: &mut Model) {
+    for (i, entry) in model.widgets.iter_mut().enumerate() {
+        ui.separator();
+
+        match entry {
+            WidgetEntry::FloatNumber(e) => {
+                let (min, max) = e.range();
+                let heading = format!("Number: {} ({}..={})", e.common().name, min, max);
+                entry_heading(ui, heading);
+                if ui
+                    .add(Slider::new(e.value_mut(), min..=max).clamp_to_range(false))
+                    .changed()
+                {
+                    if model.tether_agent.is_connected() {
+                        model
+                            .tether_agent
+                            .encode_and_publish(&e.common().plug, e.value())
+                            .expect("Failed to send number");
+                    }
+                };
+                entry_footer(ui, e);
+            }
+            WidgetEntry::WholeNumber(e) => {
+                let (min, max) = e.range();
+                let heading = format!("Number: {} ({}..={})", e.common().name, min, max);
+                entry_heading(ui, heading);
+                if ui
+                    .add(Slider::new(e.value_mut(), min..=max).clamp_to_range(false))
+                    .changed()
+                {
+                    if model.tether_agent.is_connected() {
+                        model
+                            .tether_agent
+                            .encode_and_publish(&e.common().plug, e.value())
+                            .expect("Failed to send number");
+                    }
+                };
+                entry_footer(ui, e);
+            }
+            WidgetEntry::Colour(e) => {
+                entry_heading(ui, format!("Colour: {}", e.common().name));
+                if ui
+                    .color_edit_button_srgba_unmultiplied(e.value_mut())
+                    .changed()
+                {
+                    if model.tether_agent.is_connected() {
+                        model
+                            .tether_agent
+                            .encode_and_publish(&e.common().plug, e.value())
+                            .expect("Failed to send colour")
+                    }
+                };
+                let srgba = e.value();
+                ui.label(format!(
+                    "sRGBA: {} {} {} {}",
+                    srgba[0], srgba[1], srgba[2], srgba[3],
+                ));
+                entry_footer(ui, e);
+            }
+            WidgetEntry::Bool(e) => {
+                entry_heading(ui, format!("Boolean: {}", e.common().name));
+                let checked = *e.value();
+                if ui
+                    .checkbox(
+                        e.value_mut(),
+                        format!("State: {}", {
+                            if checked {
+                                "TRUE"
+                            } else {
+                                "FALSE "
+                            }
+                        }),
+                    )
+                    .changed()
+                {
+                    if model.tether_agent.is_connected() {
+                        model
+                            .tether_agent
+                            .encode_and_publish(&e.common().plug, e.value())
+                            .expect("Failed to send boolean");
+                    }
+                }
+                entry_footer(ui, e);
+            }
+        }
+
+        if ui.button("❌ Remove").clicked() {
+            model.queue.push(QueueItem::Remove(i));
+        }
+
+        standard_spacer(ui);
+    }
+}
+
+pub fn general_agent_area(ui: &mut Ui, model: &mut Model) {
+    ui.heading("Tether Agent");
+
+    standard_spacer(ui);
+    ui.separator();
+    ui.heading("Load/Save");
+    ui.horizontal(|ui| {
+        if ui.button("Save").clicked() {
+            let text = serde_json::to_string_pretty(&model.widgets)
+                .expect("failed to serialise widget data");
+            match fs::write("./widgets.json", text) {
+                Ok(()) => {
+                    info!("Saved OK");
+                }
+                Err(e) => {
+                    error!("Error saving to disk: {:?}", e);
+                }
+            }
+        }
+        if ui.button("Load").clicked() {
+            model.widgets = load_widgets_from_disk();
+        }
+        if ui.button("Clear").clicked() {
+            model.widgets.clear();
+        }
+    });
+
+    standard_spacer(ui);
+    ui.separator();
+    ui.heading("Agent");
+
+    ui.label(model.tether_agent.broker_uri());
+
+    if model.tether_agent.is_connected() {
+        ui.label(RichText::new("Connected ☑").color(Color32::GREEN));
+    } else {
+        ui.label(RichText::new("Not connected ✖").color(Color32::RED));
+        if ui.button("Connect").clicked() {
+            model.tether_agent.connect();
+            model.insights = Insights::new(&model.tether_agent, false);
+        }
+    }
+
+    ui.separator();
+
+    ui.horizontal(|ui| {
+        ui.label("Role");
+        if ui.text_edit_singleline(&mut model.agent_role).changed() {
+            model.tether_agent.set_role(&model.agent_role);
+            model.prepare_next_entry();
+        }
+    });
+    ui.horizontal(|ui| {
+        ui.label("ID or Group");
+        if ui.text_edit_singleline(&mut model.agent_id).changed() {
+            model.tether_agent.set_id(&model.agent_id);
+            model.prepare_next_entry();
+        }
+    });
+
+    standard_spacer(ui);
+    ui.separator();
+    ui.heading("Insights");
+    ui.checkbox(&mut model.continuous_mode, "Continuous mode");
+    ui.label(format!("Topics x{}", model.insights.topics().len()));
+    for t in model.insights.topics() {
+        ui.small(t);
+    }
+    ui.separator();
+    ui.label(format!("Plug Names x{}", model.insights.plugs().len()));
+    for t in model.insights.plugs() {
+        ui.small(t);
+    }
+    ui.separator();
+    ui.label(format!("Agent Roles x{}", model.insights.roles().len()));
+    for t in model.insights.roles() {
+        ui.small(t);
+    }
+    ui.separator();
+    ui.label(format!(
+        "Agent IDs (groups) x{}",
+        model.insights.ids().len()
+    ));
+    for t in model.insights.ids() {
+        ui.small(t);
+    }
+
+    standard_spacer(ui);
+    ui.separator();
+    ui.heading(format!("Messages x{}", model.insights.message_count()));
+    if model.insights.message_log().is_empty() {
+        ui.small("0 messages received");
+    }
+    egui::ScrollArea::vertical()
+        .auto_shrink([false; 2])
+        .show(ui, |ui| {
+            for (topic, json) in model.insights.message_log().iter().rev() {
+                ui.colored_label(Color32::LIGHT_BLUE, topic);
+                ui.label(json);
+            }
+        });
+}
+
+pub fn common_widget_values(ui: &mut egui::Ui, model: &mut Model) {
+    ui.horizontal(|ui| {
+        ui.label("Name");
+        if ui
+            .text_edit_singleline(&mut model.next_widget.name)
+            .changed()
+        {
+            let shortened_name = String::from(model.next_widget.name.replace(' ', "_").trim());
+            model.next_widget.plug.name = shortened_name.clone();
+            if !model.use_custom_topic {
+                let (role, id) = model.tether_agent.description();
+                model.next_topic = format!("{role}/{id}/{}", shortened_name);
+            }
+        }
+    });
+    ui.horizontal(|ui| {
+        ui.label("Description");
+        ui.text_edit_singleline(&mut model.next_widget.description);
+    });
+    ui.horizontal(|ui| {
+        ui.label("Plug Name");
+        if ui
+            .text_edit_singleline(&mut model.next_widget.plug.name)
+            .changed()
+            && !model.use_custom_topic
+        {
+            let (role, id) = model.tether_agent.description();
+            let plug_name = model.next_widget.plug.name.clone();
+            model.next_topic = format!("{role}/{id}/{plug_name}");
+        }
+    });
+    ui.horizontal(|ui| {
+        if ui
+            .checkbox(&mut model.use_custom_topic, "Use custom topic")
+            .changed()
+            && !model.use_custom_topic
+        {
+            let (role, id) = model.tether_agent.description();
+            let plug_name = model.next_widget.plug.name.clone();
+            model.next_topic = format!("{role}/{id}/{plug_name}");
+        }
+    });
+    ui.add_enabled_ui(model.use_custom_topic, |ui| {
+        ui.horizontal(|ui| {
+            ui.label("Topic");
+            ui.text_edit_singleline(&mut model.next_topic);
+        });
     });
 }
